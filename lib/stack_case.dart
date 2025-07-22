8ca08b781f8fef86896da5bd43720424f56a0584
Ahmad Nasir <an7539661@gmail.com>
2025-07-22 16:39:20 +0500

    [Fix] Minor bug fixed

--- lib/stack_case.dart
+++ lib/stack_case.dart
@@ -1,5 +1,4 @@
 library stack_case;
 
-export 'src/stack_board_plus_items/item_case/stack_draw_case.dart';
 export 'src/stack_board_plus_items/item_case/stack_image_case.dart';
 export 'src/stack_board_plus_items/item_case/stack_text_case.dart';
--- lib/stack_items.dart
+++ lib/stack_items.dart
@@ -1,5 +1,4 @@
 library stack_items;
 
-export 'src/stack_board_plus_items/items/stack_draw_item.dart';
 export 'src/stack_board_plus_items/items/stack_image_item.dart';
 export 'src/stack_board_plus_items/items/stack_text_item.dart';
